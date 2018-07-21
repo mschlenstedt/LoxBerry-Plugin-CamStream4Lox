1ff521c8ea385d0a7ba2f9da1c79928efa2f75d2
mschlenstedt <Michael@schlenn.net>
2018-07-21 16:21:45 +0200

    Bugfixes

--- bin/ffserver.sh
+++ bin/ffserver.sh
@@ -37,17 +37,20 @@ case "$1" in
 		FFSERVERLOGLEVEL=48
 	fi
 	# Start FFServer
+	LOGEND "";
 	echo "Starting FFServer with Config ${LBPCONFIG}/${PLUGINNAME}/ffserver.conf"
 	killall ffserver > /dev/null 2>&1
 	FFREPORT=file=${FILENAME}:level=${FFSERVERLOGLEVEL} ffserver -f ${LBPCONFIG}/${PLUGINNAME}/ffserver.conf > /dev/null 2>&1  &
         exit 0
         ;;
   stop)
+	LOGEND "";
 	echo "Stopping FFServer"
 	killall ffserver > /dev/null 2>&1
         exit 0
         ;;
   *)
+	LOGEND "";
         echo "Usage: $0 [start|stop]" >&2
         exit 3
   ;;
